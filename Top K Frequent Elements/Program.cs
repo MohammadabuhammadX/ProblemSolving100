49715f5a7d62e1bc4ad73f11e5e9cdab7f78b367
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-12 17:46:13 +0300

    TopKFreq

--- Top K Frequent Elements/Program.cs
+++ Top K Frequent Elements/Program.cs
@@ -4,11 +4,33 @@
     {
         public int[] TopKFrequent(int[] nums, int k)
         {
+            Dictionary<int, int> keyValues = new Dictionary<int, int>();
 
+            foreach (int i in nums)
+            {
+                if(keyValues.ContainsKey(i)) keyValues[i]++;
+                else keyValues[i] = 1;
+            }
+            List<int[]> arr = keyValues.Select(entry => new int[] { entry.Value, entry.Key }).ToList();
+
+            arr.Sort((a,b)=>b[0].CompareTo(a[0]));
+
+            int[]res = new int[k];
+            
+            for (int i = 0; i < k; i++)
+            {
+                res[i] = arr[i][1];
+            }
+            return res;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+            int[] result = program.TopKFrequent(nums, k); 
+            Console.WriteLine(string.Join(", ", result)); 
         }
     }
 }
